11f554b25cfb88df65c25dc67f25a87bb7dab4b1
muderiz <42927576+muderiz@users.noreply.github.com>
2018-10-11 23:47:49 +0700

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	src/controllers/DepartmentController.java
    	src/controllers/EmployeeController.java
    	src/controllers/TempControllers.java
    	src/hibernate.cfg.xml

--- src/viewemp/EmployeeView.java
+++ src/viewemp/EmployeeView.java
@@ -3,10 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package view;
+package viewemp;
 
 import models.Employee;
 import org.hibernate.SessionFactory;
+import view.ChangePasswordView;
 
 /**
  *
